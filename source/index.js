9d760eb2f46bc7bbf82b5d48524f0414b4b865a3
Andrew McPherson <mcphandr@amazon.com>
2016-04-15 15:58:28 -0700

    Implemented a basic "Hello World" example.

--- source/index.js
+++ source/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, Jam Sandwich!!")
+import React from "react"
+import ReactDOM from "react-dom"
+
+import AspectRatioFrame from "./scripts/render/AspectRatioFrame.js"
+
+class Mount extends React.Component {
+    render() {
+        return (
+            <AspectRatioFrame width={40} height={30}>
+                <div>Hello World!</div>
+            </AspectRatioFrame>
+        )
+    }
+}
+
+ReactDOM.render(<Mount/>, document.getElementById("mount"))
